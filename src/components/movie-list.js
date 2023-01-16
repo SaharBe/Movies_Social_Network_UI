348e4f06f7f5e616047bb0a39d595a7ae3e191d8
SaharBe <57907970+SaharBe@users.noreply.github.com>
2023-01-16 23:08:50 +0200

    little order in the movie list

--- src/components/movie-list.js
+++ src/components/movie-list.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faX } from '@fortawesome/free-solid-svg-icons';
 import '../App.css'
 import styled from 'styled-components';
+import Table from 'react-bootstrap/Table'
+
 
 const HoverText = styled.p`
     color: #FFFFFF;
@@ -18,25 +20,58 @@ const HoverText = styled.p`
 
 function MovieList(props){
 
-    const movieClicked = movie => evt => {
-        props.movieClicked(movie)
+    // const movieClicked = movie => evt => {
+    //     props.movieClicked(movie)
+    // }
+    // const styles = StyleSheet.create({
+    //     bold: {fontWeight: 'bold'},
+    //     italic: {fontStyle: 'italic'},
+    //     underline: {textDecorationLine: 'underline'}
+    // })
+
+    const shoot = () => {
+        alert("Great Shot!");
     }
-
-
+    
     return (
         <div>
            
         <div>
           { props.movies && props.movies.map( movie =>{
-            return (<div className='squre' key={movie.id}>
-                        <h1>{movie.title}</h1>
-                        <p>{movie.language}</p>
-                        <p>{movie.budget}</p>
-                        <p>{movie.runtime}</p>
-                        <p>{movie.production_company}</p>
-                        <p>{movie.number_of_likes}</p>
-                        <FontAwesomeIcon icon={faHeart} />
-                
+            return (<div  key={movie.movieId}>
+                       
+                        <Table className='card'>
+                            <thead>
+                                <th><h1 >{movie.title}</h1></th>
+                                <th><h2>{movie.number_of_likes}<FontAwesomeIcon icon={faHeart} onClick={shoot} className={movie.user_like === "true" ? 'red' : 'other'}/></h2></th>
+                                
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><h3>Production Company:</h3></td>
+                                    <td><p>{movie.production_company}</p></td>
+                                </tr>
+                                <tr>
+                                    <td><h3>Original language:</h3></td>
+                                    <td><p>{movie.language}</p></td>
+                                </tr>
+                                <tr>
+                                    <td><h3>Budget:</h3></td>
+                                    <td><p>{movie.budget === 0 ?  <p><FontAwesomeIcon icon={faX}/></p> : <p>{movie.budget}</p>}</p></td>
+                                </tr>
+                                <tr>
+                                    <td><h3>Revenue:</h3></td>
+                                    <td><p>{movie.revenue === 0.0 ?  <p><FontAwesomeIcon icon={faX}/></p> : <p>{movie.revenue}</p>}</p></td>
+                                </tr>
+                                <tr>
+                                    <td><h3>Runtime</h3></td>
+                                    <td> <p>{movie.runtime === null? <p><FontAwesomeIcon icon={faX}/></p> : <p>movie.runtime</p>}</p></td>
+                                </tr>
+
+                                <p>{movie.user_like === "true" ? <p>I like it!</p> : <p>not liked</p>}</p>
+                                
+                            </tbody>
+                        </Table>
                     </div>)
                 
           })}
